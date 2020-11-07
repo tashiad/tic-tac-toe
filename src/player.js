58299faf4a876aec05a01cc9fffe6f52db989f64
Tashia Davis <tashiadavis10@gmail.com>
2020-11-07 11:23:55 -0700

    Create Player class draft

---
+++ src/player.js
@@ -0,0 +1,20 @@
+class Player {
+  constructor(id, token) {
+    this.id = id; //set defaults to player one since that's what we start with?
+    this.token = token; // should this be ⛷ or "skiier"?
+    this.wins = 0;
+  }
+
+  saveWinsToStorage() {
+
+  }
+
+  retreiveWinsFromStorage() {
+
+  }
+
+}
+
+// 2 Total Instantiations:
+// var player1 = new Player(1, ⛷)
+// var player2 = new Player(2, 🚵‍)
