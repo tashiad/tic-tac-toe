58bb973bea21368b98c047ae0708921e691b5178
Tashia Davis <tashiadavis10@gmail.com>
2020-11-07 18:38:47 -0700

    Add working clickSquare, buildNewBoard, and reset fns

--- src/game.js
+++ src/game.js
@@ -4,115 +4,124 @@ class Game {
     // var player2 = new Player('two', '🚵');
     this.totalToClick = [1, 2, 3, 4, 5, 6, 7, 8, 9];
     this.newBoard = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-    this.currentTurn = "one";
+    this.currentTurn = 1;
     this.winningCombo = false;
     this.tie = false;
-  }
+  };
 
-  clickSquare(player, num) {
-    player.clicked.push(num);
-    var index = this.totalToClick.indexOf(num);
+  changeTurns() { // NOT WORKING YET
+    if (this.currentTurn === 1) {
+      clickSquare(player1); // don't know what squareNum is here
+      this.currentTurn = 2;
+    } else if (this.currentTurn === 2) {
+      clickSquare(player2); // don't know what squareNum is here
+      this.currentTurn = 1;
+    };
+  };
+
+  clickSquare(player, squareNum) {
+    this.checkTotal(player);
+    player.clicked.push(squareNum);
+    this.buildNewBoard(squareNum, player.token);
+    var index = this.totalToClick.indexOf(squareNum);
     this.totalToClick.splice(index, 1);
-  }
+  };
 
-  // checkTotal(player) {
-  //   if ((this.totalToClick.length >= 5) && (this.totalToClick.length < 9)) {
-  //     this.checkForWinningCombo(player)
-  //     return 'WE HAVE A WINNER'
-  //   }
-  //   if ((this.totalToClick.length === 0) && (this.winningCombo === false)) {
-  //     this.tie = true;
-  //     return 'ITS A TIE'
-  //   }
-  // }
-
-  changeTurns() { // invoke in clickSquare? or invoke in each checkForWinningCombo condition
-    if (this.currentTurn === "one") {
-      this.currentTurn === "two";
-    } else if (this.currentTurn === "two") {
-      this.currentTurn === "one";
-    }
-  }
-
-  // REFACTOR THIS LATER?
-  checkForWinningCombo(player) {
+  checkTotal(player) { // NOT WORKING YET
+    if (this.totalToClick.length <= 5) {
+      this.checkForWinningCombo(player);
+    };
     if ((this.totalToClick.length === 0) && (this.winningCombo === false)) {
       this.tie = true;
-    } else if (player.clicked.includes(1) && player.clicked.includes(2) && player.clicked.includes(3)) {
+    };
+  };
+
+  // REFACTOR THIS LATER
+  checkForWinningCombo(player) { // NOT WORKING YET
+    if (player.clicked.includes(1) && player.clicked.includes(2) && player.clicked.includes(3)) {
       this.winningCombo = true;
       player1.isWinner = true;
       player.wins++;
-      return `${player.token} won!`;
+      player.winningBoards.push(this.newBoard);
+      reset();
     } else if (player.clicked.includes(1) && player.clicked.includes(4) && player.clicked.includes(7)) {
       this.winningCombo = true;
       player1.isWinner = true;
       player.wins++;
-      return `${player.token} won!`;
+      player.winningBoards.push(this.newBoard);
+      reset();
     } else if (player.clicked.includes(1) && player.clicked.includes(5) && player.clicked.includes(9)) {
       this.winningCombo = true;
       player1.isWinner = true;
       player.wins++;
-      return `${player.token} won!`;
+      player.winningBoards.push(this.newBoard);
+      reset();
     } else if (player.clicked.includes(2) && player.clicked.includes(5) && player.clicked.includes(8)) {
       this.winningCombo = true;
       player1.isWinner = true;
       player.wins++;
-      return `${player.token} won!`;
+      player.winningBoards.push(this.newBoard);
+      reset();
     } else if (player.clicked.includes(3) && player.clicked.includes(5) && player.clicked.includes(7)) {
       this.winningCombo = true;
       player1.isWinner = true;
       player.wins++;
-      return `${player.token} won!`;
+      player.winningBoards.push(this.newBoard);
+      reset();
     } else if (player.clicked.includes(3) && player.clicked.includes(6) && player.clicked.includes(9)) {
       this.winningCombo = true;
       player1.isWinner = true;
       player.wins++;
-      return `${player.token} won!`;
+      player.winningBoards.push(this.newBoard);
+      reset();
     } else if (player.clicked.includes(4) && player.clicked.includes(5) && player.clicked.includes(6)) {
       this.winningCombo = true;
       player1.isWinner = true;
       player.wins++;
-      return `${player.token} won!`;
+      player.winningBoards.push(this.newBoard);
+      reset();
     } else if (player.clicked.includes(7) && player.clicked.includes(8) && player.clicked.includes(9)) {
       this.winningCombo = true;
       player1.isWinner = true;
       player.wins++;
-      return `${player.token} won!`;
-    }
-  }
+      player.winningBoards.push(this.newBoard);
+      reset();
+    };
+  };
 
-  // REFACTOR THIS LATER?
-  buildNewBoard(num, token) { // invoke in clickSquare
-    if (num === 1) {
-      this.newBoard.splice(0, 1, token)
-    } else if (num === 2) {
-      this.newBoard.splice(1, 1, token)
-    } else if (num === 3) {
-      this.newBoard.splice(2, 1, token)
-    } else if (num === 4) {
-      this.newBoard.splice(3, 1, token)
-    } else if (num === 5) {
-      this.newBoard.splice(4, 1, token)
-    } else if (num === 6) {
-      this.newBoard.splice(5, 1, token)
-    } else if (num === 7) {
-      this.newBoard.splice(6, 1, token)
-    } else if (num === 8) {
-      this.newBoard.splice(7, 1, token)
-    } else if (num === 9) {
-      this.newBoard.splice(8, 1, token)
-    }
-  }
+  // REFACTOR THIS LATER
+  buildNewBoard(squareNum, token) {
+    if (squareNum === 1) {
+      this.newBoard.splice(0, 1, token);
+    } else if (squareNum === 2) {
+      this.newBoard.splice(1, 1, token);
+    } else if (squareNum === 3) {
+      this.newBoard.splice(2, 1, token);
+    } else if (squareNum === 4) {
+      this.newBoard.splice(3, 1, token);
+    } else if (squareNum === 5) {
+      this.newBoard.splice(4, 1, token);
+    } else if (squareNum === 6) {
+      this.newBoard.splice(5, 1, token);
+    } else if (squareNum === 7) {
+      this.newBoard.splice(6, 1, token);
+    } else if (squareNum === 8) {
+      this.newBoard.splice(7, 1, token);
+    } else if (squareNum === 9) {
+      this.newBoard.splice(8, 1, token);
+    };
+  };
 
-  reset() { // invoke in each checkForWinningCombo condition
+  reset() {
     this.totalToClick = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-    this.currentTurn = 'one';
+    this.newBoard = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    this.currentTurn = 1;
     this.winningCombo = false;
     this.tie = false;
     player1.clicked = [];
     player2.clicked = [];
     player1.isWinner = false;
     player2.isWinner = false;
-  }
+  };
 
-}
+};
