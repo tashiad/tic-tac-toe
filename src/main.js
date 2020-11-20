a5fd6c0365f6efd5e6e5e805c9d0a02a27f386b9
Tashia Davis <tashiadavis10@gmail.com>
2020-11-20 16:17:06 -0700

    Refactor checkForWinningCombo per instructor feedback

--- src/main.js
+++ src/main.js
@@ -17,17 +17,17 @@ function identifySquare(event) {
   if (event.target.classList.contains('gameboard-square')) {
     event.target.classList.add('disabled');
     clickSquare(event.target);
-  };
-};
+  }
+}
 
 function clickSquare(square) {
   if (game.currentTurn === 1) {
     executePlayer1Turn(square);
   } else if (game.currentTurn === 2) {
     executePlayer2Turn(square);
-  };
+  }
   loadWins();
-};
+}
 
 function executePlayer1Turn(square) {
   var idNum = square.id[square.id.length - 1];
@@ -35,7 +35,7 @@ function executePlayer1Turn(square) {
   gameState.innerText = `It's ${game.player2.token}'s turn`;
   game.updateClickedSquares(game.player1, idNum);
   changeGameState(game.player1);
-};
+}
 
 function executePlayer2Turn(square) {
   var idNum = square.id[square.id.length - 1];
@@ -43,29 +43,29 @@ function executePlayer2Turn(square) {
   gameState.innerText = `It's ${game.player1.token}'s turn`;
   game.updateClickedSquares(game.player2, idNum);
   changeGameState(game.player2);
-};
+}
 
 function changeGameState(player) {
   if (game.tie) {
     gameState.innerText = `It's a draw!`;
     endGame();
-  };
+  }
   if (player.isWinner) {
     gameState.innerText = `${player.token} wins!`;
     endGame();
-  };
-};
+  }
+}
 
 function endGame() {
   disableSquare();
   setTimeout(resetBoard, 3000);
-};
+}
 
 function disableSquare() {
   for (var i = 0; i < squares.length; i++) {
     squares[i].classList.add('disabled');
-  };
-};
+  }
+}
 
 function resetBoard() {
   game.reset();
@@ -73,40 +73,40 @@ function resetBoard() {
   for (var i = 0; i < squares.length; i++) {
     squares[i].innerText = '';
     squares[i].classList.remove('disabled');
-  };
-};
+  }
+}
 
 function determineTurnAfterGameEnd() {
   if (game.currentTurn === 1) {
     gameState.innerText = `It's ${game.player1.token}'s turn`;
   } else if (game.currentTurn === 2) {
     gameState.innerText = `It's ${game.player2.token}'s turn`;
-  };
-};
+  }
+}
 
 function loadWins() {
   game.player1.retrieveWinsFromStorage(game.player1);
   game.player2.retrieveWinsFromStorage(game.player2);
   updateWins();
-};
+}
 
 function updateWins() {
   checkForSkiierWins();
   checkForBikerWins();
-};
+}
 
 function checkForSkiierWins() {
   if (game.player1.wins === null) {
     skiier.innerText = `0 wins`;
   } else {
     skiier.innerText = `${game.player1.wins} wins`;
-  };
-};
+  }
+}
 
 function checkForBikerWins() {
   if (game.player2.wins === null) {
     biker.innerText = `0 wins`;
   } else {
     biker.innerText = `${game.player2.wins} wins`;
-  };
-};
+  }
+}
--- src/player.js
+++ src/player.js
@@ -5,27 +5,27 @@ class Player {
     this.clicked = [];
     this.wins = 0;
     this.isWinner = false;
-  };
+  }
 
   saveWinsToStorage(player) {
     if (player.id === 'one') {
       localStorage.setItem('player1Wins', JSON.stringify(this.wins));
-    };
+    }
 
     if (player.id === 'two') {
       localStorage.setItem('player2Wins', JSON.stringify(this.wins));
-    };
-  };
+    }
+  }
 
   retrieveWinsFromStorage(player) {
     var player1SavedWins = JSON.parse(localStorage.getItem('player1Wins'));
     var player2SavedWins = JSON.parse(localStorage.getItem('player2Wins'));
     if (player.id === 'one') {
       this.wins = player1SavedWins;
-    };
+    }
     if (player.id === 'two') {
       this.wins = player2SavedWins;
-    };
-  };
+    }
+  }
 
-};
+}
